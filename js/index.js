d0963fd2d5a514465cad987c310efd5c17aad6c4
anouther <155570107+anouther@users.noreply.github.com>
2025-01-29 23:32:48 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hi, pretty serabelle'.split('')
+const text = 'Hi, beye'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
